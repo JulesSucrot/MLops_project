99b538aa3095cff3ea78385ec736de0dca7851b2
JulesSucrot <jules.sucrot@efrei.net>
2025-09-14 21:20:27 +0200

    deploy ps1 update

--- deploy/deploy.ps1
+++ deploy/deploy.ps1
@@ -1,13 +1,23 @@
-# deploy/deploy.ps1
 $ErrorActionPreference = "Stop"
+
+# Lire depuis l'env
 $IMAGE = $env:DOCKER_IMAGE
 if (-not $IMAGE) { throw "DOCKER_IMAGE non défini" }
 
-docker pull $IMAGE | Out-Host
-docker rm -f hogwarts 2>$null | Out-Null
+# Facultatif: logs
+Write-Host "Deploy image = $IMAGE"
 
-docker run -d --name hogwarts -p 8000:8000 `
+# Pull
+docker pull $IMAGE
+
+# Stop / remove si déjà lancé
+docker stop hogwarts 2>$null | Out-Null
+docker rm hogwarts 2>$null   | Out-Null
+
+# Run
+docker run -d --rm --name hogwarts -p 8000:8000 `
   -e MLFLOW_TRACKING_URI=$env:MLFLOW_TRACKING_URI `
   -e MLFLOW_TRACKING_USERNAME=$env:MLFLOW_TRACKING_USERNAME `
   -e MLFLOW_TRACKING_PASSWORD=$env:MLFLOW_TRACKING_PASSWORD `
-  $IMAGE | Out-Host
+  -e MODEL_URI=$env:MODEL_URI `
+  $IMAGE
